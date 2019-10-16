0658bd392946f6526e582eb3d5e85d97c2ad4eca
Cody Nicoll <cdnicoll@gmail.com>
2019-10-15 21:29:44 -0700

    🌟 added a fix to address Unexpected Token Export

--- server/index.js
+++ server/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
+
+// TODO: Make this an index file
+const goodreads = require('./lib/goodreads/index.js')
+
  app
   .use(cors())
   .use(morgan('combined'));
  app.get("/", (req, res) => {
+   console.log(goodreads)
     res.send("Hello World");
 });
 app.listen(process.env.PORT || 3001, function() {
